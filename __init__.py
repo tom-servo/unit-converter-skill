34c1888fb6eba68798dc22bc4af54f04512cab9b
tom-servo <kc.tom.servo@gmail.com>
2019-06-25 13:51:50 -0700

    Add initial unit conversion implementation.

--- __init__.py
+++ __init__.py
@@ -1,14 +1,26 @@
-from mycroft import MycroftSkill, intent_file_handler
-
+from mycroft import MycroftSkill, intent_file_handler, util as mutil
+from pint import UnitRegistry
 
 class UnitConverter(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
+        self.units = UnitRegistry()
+        self.Q_ = self.units.Quantity
+        
     @intent_file_handler('converter.unit.intent')
     def handle_converter_unit(self, message):
-        self.speak_dialog('converter.unit')
-
+        d = message.data
+        (firstu, secondu, seconda) = (d['firstunit'], d['secondunit'], d['secondamount'])
+        seconda = '1' if seconda == "a" else seconda
+        qfrom = self.Q_(seconda + " * " + secondu);
+        qTo = qfrom.to(firstu)
+        resDict = {
+            'firstAmount': mutil.nice_number(qTo.magnitude),
+            'firstUnit': firstu,
+            'secondAmount': seconda,
+            'secondUnit': secondu
+        }
+        self.speak_dialog('converter.unit', resDict)
 
 def create_skill():
     return UnitConverter()
