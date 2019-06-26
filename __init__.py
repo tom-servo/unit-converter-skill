07e54e61c52040e05c04c84e99db6758779c7739
tom-servo <kc.tom.servo@gmail.com>
2019-06-26 13:00:18 -0700

    Add exception and handling a negative test.

--- __init__.py
+++ __init__.py
@@ -9,18 +9,21 @@ class UnitConverter(MycroftSkill):
         
     @intent_file_handler('converter.unit.intent')
     def handle_converter_unit(self, message):
-        d = message.data
-        (firstu, secondu, seconda) = (d['firstunit'], d['secondunit'], d['secondamount'])
-        seconda = '1' if seconda == "a" else seconda
-        qfrom = self.Q_(seconda + " * " + secondu);
-        qTo = qfrom.to(firstu)
-        resDict = {
-            'firstAmount': mutil.nice_number(qTo.magnitude),
-            'firstUnit': firstu,
-            'secondAmount': seconda,
-            'secondUnit': secondu
-        }
-        self.speak_dialog('converter.unit', resDict)
+        try:
+            d = message.data
+            (firstu, secondu, seconda) = (d['firstunit'], d['secondunit'], d['secondamount'])
+            seconda = '1' if seconda == "a" else seconda
+            qfrom = self.Q_(seconda + " * " + secondu);
+            qTo = qfrom.to(firstu)
+            resDict = {
+                'firstAmount': mutil.nice_number(qTo.magnitude),
+                'firstUnit': firstu,
+                'secondAmount': seconda,
+                'secondUnit': secondu
+            }
+            self.speak_dialog('converter.unit', resDict)
+        except:
+            self.speak_dialog('converter.unit.failed')
 
 def create_skill():
     return UnitConverter()
